a20c8d319b0760d913c343c193fc7ace010d314e
iyinchao <yincharles@163.com>
2018-01-28 04:05:46 +0800

    更新动画

--- js/ui.js
+++ js/ui.js
@@ -1,15 +1,52 @@
+var introLogoAni = -1
+
 function initVue() {
     const app = new Vue({
         template: '#vue-root',
         data: function () {
             return {
-                state: 'intro'
+                state: 'intro',
+                introActive: false
             }
         },
-        methods : {
-          setState : function(stateName) {
-            this.state = stateName;
-          },
+        watch: {
+            state: function(nState, oState) {
+                this.onStateChange(nState, oState)
+            }
+        },
+        methods: {
+            onStateChange(nState, oState) {
+                if (nState === 'intro') {
+                    clearTimeout(introLogoAni)
+                    setTimeout(() => {
+                        this.introActive = true
+                    }, 300)
+                }
+            },
+            onWindowResizeThrottle () {
+                if (document.body.clientHeight > document.body.clientWidth) {
+                    this.$refs.logoWrapper.style.height = '100vw';
+                    this.$refs.logoWrapper.style.width = '100vw';
+                } else {
+                    this.$refs.logoWrapper.style.height = '100vh';
+                    this.$refs.logoWrapper.style.width = '100vh';
+                }
+            },
+            setState : function(stateName) {
+                this.state = stateName;
+            },
+        },
+        mounted () {
+            if (this.state === 'intro') {
+                this.onStateChange(this.state)
+            }
+
+            this.onWindowResizeThrottle()
+
+            window.addEventListener('resize', this.onWindowResizeThrottle)
+        },
+        beforeDestroy () {
+            window.removeEventListener('resize', this.onWindowResizeThrottle)
         }
     })
     
